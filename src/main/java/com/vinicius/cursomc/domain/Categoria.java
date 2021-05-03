47e06d6ef76f1033a05b15f45ab4ad257c0f8415
Vinicius Riemma <viniciusriemma@gmail.com>
2021-05-03 09:02:40 -0300

    Produto e associaco muitos para muitos

--- src/main/java/com/vinicius/cursomc/domain/Categoria.java
+++ src/main/java/com/vinicius/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.vinicius.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +19,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 		
 	public Categoria() {
 			
@@ -42,6 +48,14 @@ public class Categoria implements Serializable {
 		public void setNome(String nome) {
 			this.nome = nome;
 		}
+		
+		public List<Produto> getProdutos() {
+			return produtos;
+		}
+
+		public void setProdutos(List<Produto> produtos) {
+			this.produtos = produtos;
+		}
 
 		@Override
 		public int hashCode() {
@@ -67,6 +81,8 @@ public class Categoria implements Serializable {
 				return false;
 			return true;
 		}
+
+
 		
 		
 	
